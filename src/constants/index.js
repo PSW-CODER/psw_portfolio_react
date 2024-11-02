77e181caa2df96c9a4131b19e7d1e47cd233db4e
PSW-CODER <pyaesone096553@gmail.com>
2024-11-02 15:19:16 +0630

    Public

---
+++ src/constants/index.js
@@ -0,0 +1,21 @@
+import project1 from "../assets/movie-web.jpg";
+
+export const HERO_CONTENT = `I am a backend developer. I have honed my skills in front-end technologies like HTML, CSS, JavaScript, React, Bootstrap and Tailwind, as well as back-end technologies like PHP, Laravel, MySQL. My goal is to improve my programming skill and I want to create RealWorld Projects using my skill.`
+
+export const ABOUT_TEXT = `Hi Everyone, My name is Pyae Sone Win. I have achieved SQA Advenced Diploma from KMD Institute in 2020. I learned Programming Concept like OOP, Binary Operator, Logic Gate, and back-end technologies like C++ and PHP, as well as front-end technologies like HTML, CSS. After learning, I studied Laravel, React, Bootstrap, Tailwind from Online.`
+
+export const PROJECTS = [
+    {
+        title: "Movie Website",
+        image: project1,
+        description: 
+            "A fully functional movie website with features like movie listing, request plan, and user authentication.",
+        technologies: ["Laravel", "Bootstrap", "MySQL"],
+    },
+]
+
+export const CONTACT = {
+    address: "Yangon, Hlaing Tharyar",
+    phoneNo: "+95 9764 866 949",
+    email: "pyaesone096553@gmail.com",
+}
